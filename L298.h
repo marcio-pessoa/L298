ebb9399fa79b12d235f03c05ed170fa44030bc9f
Márcio Pessoa <marcio.pessoa@sciemon.com>
2018-10-17 17:08:22 -0300

    Minor updates

--- L298.h
+++ L298.h
@@ -1,6 +1,6 @@
 /* L298.h - L298 DC Motor Control Library
  * 
- * Author: Márcio Pessoa <marcio.pessoa@sciemon.com>
+ * Author: Márcio Pessoa <marcio.pessoa@gmail.com>
  * Contributors: none
  * 
  * Change log
